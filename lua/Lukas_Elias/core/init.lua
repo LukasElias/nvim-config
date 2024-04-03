645ddd1f9314977d81436e61e067ad021a587cc4
LukasElias <lukaslundmajland@gmail.com>
2024-04-03 20:17:42 +0200

    Configured my mappings and options

--- lua/Lukas_Elias/core/init.lua
+++ lua/Lukas_Elias/core/init.lua
@@ -3,9 +3,39 @@ require("Lukas_Elias.core.map")
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.o.relativenumber = true
+vim.opt.mouse = 'a'
 
-vim.o.autoindent = true
-vim.o.expandtab = false
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.autoindent = true
+vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.wo.signcolumn = 'yes'
+
+vim.opt.updatetime = 750
+vim.opt.timeoutlen = 300
+
+vim.opt.completeopt = {
+	'menuone',
+	'noinsert',
+	'noselect'
+}
+
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
--- lua/Lukas_Elias/core/map.lua
+++ lua/Lukas_Elias/core/map.lua
@@ -1,1 +1,8 @@
 vim.keymap.set('n', 'H', ':noh<cr>')
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
